bcb46b2a5ab68b9c218f49df683a6f12fb0b4f2c
Juan C Nuno <juancnuno@gmail.com>
2023-12-27 22:17:05 -0800

    Add toEnumMultiset

--- src/main/java/com/juancnuno/adventofcode/Multisets.java
+++ src/main/java/com/juancnuno/adventofcode/Multisets.java
@@ -1,5 +1,6 @@
 package com.juancnuno.adventofcode;
 
+import java.util.EnumMap;
 import java.util.Map;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -11,6 +12,10 @@ public final class Multisets {
 
     public static <K> Collector<K, ?, Map<K, Integer>> toMultiset() {
         return Collectors.toMap(key -> key, key -> 1, Integer::sum);
+    }
+
+    public static <K extends Enum<K>> Collector<K, ?, Map<K, Integer>> toEnumMultiset(Class<K> c) {
+        return Collectors.toMap(key -> key, key -> 1, Integer::sum, () -> new EnumMap<>(c));
     }
 
     public static <K> void put(Map<K, Integer> multiset, K key, int count) {
